36cd7f3d76ff2611f36477406e2dc5428b0e17e9
mohamedAlaa <mohamedalhakar5@gmail.com>
2024-07-18 05:03:24 +0300

    solve eror4

--- app/components/ExploreCard.tsx
+++ app/components/ExploreCard.tsx
@@ -1,13 +1,13 @@
 import Image from "next/image"
 import { ExploreItem } from "../types/app"
 
-type ExploreProps=ExploreItem
+type ExploreProps=ExploreItem;
 
 function ExploreCard({location,distance,img}:ExploreProps) {
   return (
     <div className="flex items-center m-2 mt-5 space-x-4 cursor-pointer hover:bg-gray-100 hover:scale-105 transition transform  duration-200 ease-out">
     <div className="relative w-16 h-16 ">
-      <Image src={image} alt="ExploreCard-Img" fill/>
+      <Image src={img} alt="ExploreCard-Img" fill/>
     </div>
     <div>
       <h2>{location}</h2>
