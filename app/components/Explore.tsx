ac595bff71fa16a206913c4e6c2c4ad3f97008d7
mohamedAlaa <mohamedalhakar5@gmail.com>
2024-07-18 04:51:02 +0300

    solve eror3

--- app/components/Explore.tsx
+++ app/components/Explore.tsx
@@ -10,7 +10,7 @@ async function Explore() {
             <div className="container">
                 <h2 className='text-4xl font-semibold mb-5'>Explore nearby</h2>
                <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 ">
-                 {exploreData.map((item,index) => {
+                 {exploreData.map((item) => {
                    return <ExploreCard key={item.img} image={item.img} distance={item.distance} location={item.location} />
                 })}
                </div>
